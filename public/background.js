9a054aa1abd58ae02586380f26aaf2a5b300c0c1
sensurusirupus <sensurusirupus@gmail.com>
2024-07-01 10:00:26 +0000

    refactor: remove signTransaction dummy function
    Remove placeholder logic for transaction signing from the signTransaction function in background.js. Previously, a dummy function was used as a placeholder until the actual transaction signing logic was implemented. With this change, the function will remain empty, signaling readiness for the impending implementation of the real signing logic. No functional changes arise as a result of this adjustment, it merely paves the way for further development.
    
    Resolves #123

--- public/background.js
+++ public/background.js
@@ -4,15 +4,11 @@ chrome.runtime.onConnectExternal.addListener((port) => {
       // Handle DApp connection request
       port.postMessage({ type: "CONNECTED" });
     } else if (msg.type === "SIGN_TRANSACTION") {
-      // Handle transaction signing request
       const { transaction } = msg;
-      // Sign the transaction
       const signedTransaction = signTransaction(transaction); // Implement signTransaction function
       port.postMessage({ type: "SIGNED_TRANSACTION", signedTransaction });
     }
   });
 });
 
-function signTransaction(transaction) {
-  // Implement your transaction signing logic here
-}
+function signTransaction(transaction) {}
